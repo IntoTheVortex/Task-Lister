c43a43221a4d32b01707d3215533d94e42e24bfc
IntoTheVortex <amberadelaideshore@gmail.com>
2020-12-17 10:25:50 -0800

    cleanup

--- src/TaskListInterface.java
+++ src/TaskListInterface.java
@@ -11,11 +11,13 @@ import java.util.Vector;
 public class TaskListInterface {
     private JPanel rootPanel;
     private JList list1;
-    private JSpinner spinner1;
+    private JSpinner listSpinner;
     private JButton randNumGen;
     private JTextField inputTask;
-    private JButton enterTask;
+    private JButton enterTaskButton;
     private JLabel taskLabel;
+    private JButton deleteButton;
+    private JLabel spinnerLabel;
     private final Color purple = new Color(47, 1, 90);
 
     Vector<String> tasks = new Vector<String>();
@@ -31,9 +33,10 @@ public class TaskListInterface {
         rootPanel.setPreferredSize(new Dimension(500,800));
         rootPanel.setBackground(purple);
 
-        setUpSpinner();
+        //setUpSpinner(); don't do til action, can't use empty list. TODO: Or try catch?
         setUpList();
         taskLabel.setForeground(Color.WHITE);
+        spinnerLabel.setForeground(Color.WHITE);
 
         //button setup:
         randNumGen.addActionListener(new ActionListener() {
@@ -44,22 +47,36 @@ public class TaskListInterface {
             }
         });
 
-        enterTask.addActionListener(new ActionListener(){ //works on 'enter'
+        enterTaskButton.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
                 tasks.add(inputTask.getText());
-                JOptionPane.showMessageDialog(rootPanel, tasks.toString());
                 inputTask.setText(null);
                 //then update JList
                 setUpList();
+                setUpSpinner();
 
+            }
+        });
+
+        deleteButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //TODO mostly implemented, but leaves last task in spinner even when deleted
+
+                if (tasks.contains(listSpinner.getValue())){
+                    tasks.remove(listSpinner.getValue());
+                    setUpList();
+                    setUpSpinner();
+                }
             }
         });
     }
 
     /**
      * Set up task list
-     * Simple version - TODO implement changeable version, saving, action listeners etc
+     * Simple version
      */
     private void setUpList(){
         String[] taskList = new String[tasks.size()];
@@ -85,8 +102,12 @@ public class TaskListInterface {
      * Set up the spinner model and characteristics
      */
     private void setUpSpinner(){
-        SpinnerModel numTasks = new SpinnerNumberModel(1, 0,8,1);
-        spinner1.setModel(numTasks);
+        String[] taskList = new String[tasks.size()];
+        for(int i = 0; i < tasks.size(); i++) {
+            taskList[i] = tasks.elementAt(i);
+        }
+        SpinnerModel taskSpinner = new SpinnerListModel(taskList);
+        listSpinner.setModel(taskSpinner);
     }
 
     /**
